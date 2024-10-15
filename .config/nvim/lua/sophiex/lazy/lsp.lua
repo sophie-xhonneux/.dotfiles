25a5afa0e7f1c11a94e163d1bff6dd2e073f9229
sophiex <24638638+sophie-xhonneux@users.noreply.github.com>
2024-10-15 19:22:52 -0400

    Fix lsp config to enable formattting with ruff

--- .config/nvim/lua/sophiex/lazy/lsp.lua
+++ .config/nvim/lua/sophiex/lazy/lsp.lua
@@ -3,6 +3,7 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
+        'VonHeikemen/lsp-zero.nvim',
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
@@ -11,69 +12,14 @@ return {
     },
     config = function()
         local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
-
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                pylsp,
-            },
-            handlers = {
-                function(server_name) -- default handler (optional)
-
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end
-            }
-        })
-
-        local lspconfig = require("lspconfig")
-        lspconfig.pylsp.setup({
-            capabilities = capabilities,
-            settings = {
-                pylsp = {
-                    plugins = {
-                        -- type checker
-                        pylsp_mypy = {enabled = true},
-                        -- formater & linter
-                        ruff = {
-                          enabled = true,  -- Enable the plugin
-                          formatEnabled = true,  -- Enable formatting using ruffs formatter
-                          extendSelect = { "I" },  -- Rules that are additionally used by ruff
-                          extendIgnore = { "C90" },  -- Rules that are additionally ignored by ruff
-                          format = { "I" },  -- Rules that are marked as fixable by ruff that should be fixed when running textDocument/formatting
-                          severities = { ["D212"] = "I" },  -- Optional table of rules where a custom severity is desired
-                          unsafeFixes = false,  -- Whether or not to offer unsafe fixes as code actions. Ignored with the "Fix All" action
-
-                          -- Rules that are ignored when a pyproject.toml or ruff.toml is present:
-                          lineLength = 88,  -- Line length to pass to ruff checking and formatting
-                          exclude = { "__about__.py" },  -- Files to be excluded by ruff checking
-                          select = { "F" },  -- Rules to be enabled by ruff
-                          ignore = { "D210" },  -- Rules to be ignored by ruff
-                          perFileIgnores = { ["__init__.py"] = "CPY001" },  -- Rules that should be ignored for specific files
-                          preview = true,  -- Whether to enable the preview style linting and formatting.
-                          targetVersion = "py310",  -- The minimum python version to target (applies for both linting and formatting).
-                        },
-                    }
-                }
-            }
-        })
-        lspconfig.ocamllsp.setup({
-            on_attach = on_attach,
-        })
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
-        
+
         cmp.setup({
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                 ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-e>'] = cmp.mapping.abort(),
                 ["<C-y>"] = cmp.mapping.complete(),
             }),
             sources = cmp.config.sources({
@@ -95,7 +41,97 @@ return {
             },
         })
 
-        vim.keymap.set('n', '<leader>hh', vim.lsp.buf.hover, {})
+        require('mason').setup({
+            PATH = "append"
+        })
+        require('mason-lspconfig').setup({
+            ensure_installed = { 'lua_ls', 'ruff', 'pylsp'},
+            handlers = {
+                function(server_name)
+                    if server_name == 'ruff' then
+                        local ruff_config_path = vim.loop.os_homedir() .. '/.config/ruff/ruff.toml'
+                        local project_ruff_config = vim.loop.cwd() .. '/pyproject.toml'
+                        local f = io.open(project_ruff_config, 'r')
+                        if f ~= nil then
+                            io.close(f)
+                            ruff_config_path = project_ruff_config
+                        end
+                        require('lspconfig').ruff.setup({
+                            init_options = {
+                                settings = {
+                                    format = {
+                                        args = { "--config=" .. ruff_config_path }
+                                    },
+                                    lint = {
+                                        args = { "--config=" .. ruff_config_path }
+                                    }
+                                }
+                            }
+                        })
+                    elseif server_name == 'pylsp' then
+                        require('lspconfig').pylsp.setup({
+                            settings = {
+                                pylsp = {
+                                    plugins = {
+                                        pylsp_mypy = {enabled = true},
+                                        flake8 = { enabled = false },
+                                        yapf = { enabled = false },
+                                        flakes = { enabled = false },
+                                        pylint = { enabled = false },
+                                        pycodestyle = { enabled = false },
+                                        pydocstyle = { enabled = false },
+                                        mccabe = { enabled = false },
+                                        autopep8 = { enabled = false },
+                                    }
+                                }
+                            }
+                        })
+                    elseif server_name == 'lua_ls' then
+                        require('lspconfig').lua_ls.setup({
+                            settings = {
+                                Lua = {
+                                    diagnostics = {
+                                        globals = { 'vim', 'describe', 'it' },
+                                    },
+                                    workspace = { library = vim.api.nvim_get_runtime_file("", true), },
+                                    telemetry = { enable = false, },
+                                }
+                            }
+                        })
+                    else
+                        require('lspconfig')[server_name].setup({
+                        })
+                    end
+                end,
+            }
+        })
+        require('lspconfig').ocamllsp.setup({
+            on_attach = on_attach,
+        })
 
+        vim.api.nvim_create_autocmd("LspAttach", {
+            callback = function(event
+            )
+                local map = function(key, func, desc)
+                    vim.keymap.set("n", key, func, { buffer = event.buf, desc = "LSP: " .. desc })
+                end
+
+                map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+                map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+                map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+                map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+                map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+                map(
+                    "<leader>ws",
+                    require("telescope.builtin").lsp_dynamic_workspace_symbols,
+                    "[W]orkspace [S]ymbols"
+                )
+                map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+                map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+                map("<leader>hh", vim.lsp.buf.hover, "Hover Documentation")
+                map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+                vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+            end,
+        })
     end
 }
